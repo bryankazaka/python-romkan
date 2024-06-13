925075130764d2c023551fff05f1f3339213bfc3
Bryan Kazaka <117994759+bryankazaka@users.noreply.github.com>
2024-06-13 14:52:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import json
 import importlib.util
 
 here = os.path.abspath(os.path.dirname(__file__))
-proj_info = json.loads(open(os.path.join(here, 'PROJ_METADATA')).read())
+proj_info = json.loads(open(os.path.join(here, PROJ_METADATA)).read())
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst')).read()
 
@@ -22,27 +22,27 @@ VERSION = version.__version__
 
 from setuptools import setup, find_packages
 setup(
-    name = proj_info['name'],
-    version = VERSION,
+    name=proj_info['name'],
+    version=VERSION,
     
-    author = proj_info['author'],
-    author_email = proj_info['author_email'],
-    url = proj_info['url'],
-    license = proj_info['license'],
+    author=proj_info['author'],
+    author_email=proj_info['author_email'],
+    url=proj_info['url'],
+    license=proj_info['license'],
     
-    description = proj_info['description'],
-    keywords = proj_info['keywords'],
+    description=proj_info['description'],
+    keywords=proj_info['keywords'],
     
-    long_description = README + '\n\n' + CHANGELOG,
+    long_description=README + '\n\n' + CHANGELOG,
     
-    packages = find_packages('src'),
-    package_dir = {'' : 'src'},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     
-    test_suite = 'tests',
+    test_suite='tests',
     
-    platforms = 'any',
-    zip_safe = False,
-    include_package_data = True,
+    platforms='any',
+    zip_safe=False,
+    include_package_data=True,
     
-    classifiers = proj_info['classifiers'],
+    classifiers=proj_info['classifiers'],
 )
